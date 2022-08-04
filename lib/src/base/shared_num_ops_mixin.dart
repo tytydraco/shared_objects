afc8eb43efb4126684c7eca999326f023d61415c
Tyler Nijmeh <tylernij@gmail.com>
2022-08-04 13:25:03 -0700

    Make data mutable via function
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/base/shared_num_ops_mixin.dart
+++ lib/src/base/shared_num_ops_mixin.dart
@@ -6,51 +6,44 @@ import 'package:shared_objects/shared_objects.dart';
 mixin SharedNumOpsMixin<T extends num> on SharedObjectBase<T> {
   /// Add a numerical value to the current value.
   Future<void> add(T value) async {
-    final initial = await get();
-    if (initial == null) return;
-    final result = initial + value;
-    await set(result as T);
+    await mutate((initial) {
+      if (initial == null) return initial;
+      return initial + value as T;
+    });
   }
 
   /// Subtract a numerical value from the current value.
   Future<void> sub(T value) async {
-    final initial = await get();
-    if (initial == null) return;
-    final result = initial - value;
-    await set(result as T);
+    await mutate((initial) {
+      if (initial == null) return initial;
+      return initial - value as T;
+    });
   }
 
   /// Multiply a numerical value by the current value.
   Future<void> mul(T value) async {
-    final initial = await get();
-    if (initial == null) return;
-    final result = initial * value;
-    await set(result as T);
+    await mutate((initial) {
+      if (initial == null) return initial;
+      return initial * value as T;
+    });
   }
 
   /// Divide a numerical value from the current value.
   ///
   /// If [T] is an [int], use integer division.
   Future<void> div(T value) async {
-    final initial = await get();
-    if (initial == null) return;
-
-    // Use integer division if dealing with integers.
-    final num result;
-    if (T == int) {
-      result = initial ~/ value;
-    } else {
-      result = initial / value;
-    }
-
-    await set(result as T);
+    await mutate((initial) {
+      if (initial == null) return initial;
+      if (T == int) return initial ~/ value as T;
+      return initial / value as T;
+    });
   }
 
   /// Raise the current value to a numerical value exponent.
   Future<void> pow(T value) async {
-    final initial = await get();
-    if (initial == null) return;
-    final result = math.pow(initial, value);
-    await set(result as T);
+    await mutate((initial) {
+      if (initial == null) return initial;
+      return math.pow(initial, value) as T;
+    });
   }
 }
--- lib/src/base/shared_object_base.dart
+++ lib/src/base/shared_object_base.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// An extensible class that persists an object to [SharedPreferences].
 abstract class SharedObjectBase<T> {
   /// Creates a new [SharedObjectBase] given an [id].
-  SharedObjectBase(this.id);
+  const SharedObjectBase(this.id);
 
   /// A unique [SharedPreferences] key.
   final String id;
@@ -15,4 +15,14 @@ abstract class SharedObjectBase<T> {
 
   /// Get the value of this object using [SharedPreferences].
   Future<T?> get();
+
+  /// Modify the current value given a handler function.
+  ///
+  /// The handler function is provided the current value, and returns a new,
+  /// updated value.
+  Future<void> mutate(T? Function(T? initial) handler) async {
+    final initial = await get();
+    final result = handler(initial);
+    if (initial != result) await set(result);
+  }
 }
