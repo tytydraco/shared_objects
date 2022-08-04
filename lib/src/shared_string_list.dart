0e21e0cd0451ebc8d71d4a4ec0136d920570eec0
Tyler Nijmeh <tylernij@gmail.com>
2022-08-04 12:20:46 -0700

    Add String list
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ lib/src/shared_string_list.dart
@@ -0,0 +1,26 @@
+import 'package:shared_objects/src/shared_object_base.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// A [List] of [String]s that is directly integrated with [SharedPreferences].
+class SharedStringList extends SharedObjectBase<List<String>> {
+  /// Creates a new [SharedStringList] given an [id].
+  SharedStringList(super.id);
+
+  final _sharedPrefs = SharedPreferences.getInstance();
+
+  @override
+  Future<void> set(List<String>? value) async {
+    final instance = await _sharedPrefs;
+    if (value == null) {
+      await instance.remove(id);
+    } else {
+      await instance.setStringList(id, value);
+    }
+  }
+
+  @override
+  Future<List<String>?> get() async {
+    final instance = await _sharedPrefs;
+    return instance.getStringList(id);
+  }
+}
---
+++ test/src/shared_string_list_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_objects/src/shared_string_list.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  group('Shared string list', () {
+    SharedPreferences.setMockInitialValues({});
+    final sharedStringList = SharedStringList('my_string_list');
+
+    test('Initialized to null', () async {
+      expect(
+        await sharedStringList.get(),
+        null,
+      );
+    });
+
+    test('Set new value', () async {
+      await expectLater(
+        () => sharedStringList.set(['1', '2', '3']),
+        returnsNormally,
+      );
+    });
+
+    test('Ensure get() is accurate with shared preferences', () async {
+      final sharedPrefs = await SharedPreferences.getInstance();
+
+      expect(
+        sharedPrefs.getStringList('my_string_list'),
+        await sharedStringList.get(),
+      );
+    });
+
+    test('Ensure stored value is accurate', () async {
+      expect(
+        await sharedStringList.get(),
+        ['1', '2', '3'],
+      );
+    });
+
+    test('Delete value', () async {
+      await sharedStringList.set(null);
+
+      expect(
+        await sharedStringList.get(),
+        null,
+      );
+    });
+  });
+}
