98516ccf199f779e65c61bd4e10f2a098517a5e4
Tyler Nijmeh <tylernij@gmail.com>
2022-08-09 15:22:13 -0700

    Add getOrElse and getNotNull
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/base/shared_object_base.dart
+++ lib/src/base/shared_object_base.dart
@@ -16,6 +16,15 @@ abstract class SharedObjectBase<T> {
   /// Get the value of this object using [SharedPreferences].
   Future<T?> get();
 
+  /// Get the value of this object using [SharedPreferences]. If the value is
+  /// null, return the value from the [orElse] handler instead.
+  Future<T> getOrElse(T Function() orElse) async => await get() ?? orElse();
+
+  /// Get the non-nullable value of this object using [SharedPreferences].
+  ///
+  /// Only use this method if the value is guaranteed to not be null.
+  Future<T> getNotNull() async => (await get())!;
+
   /// Modify the current value given a handler function.
   ///
   /// The handler function is provided the current value, and returns a new,
